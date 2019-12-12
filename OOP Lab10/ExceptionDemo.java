5776dc35191f69fcc50390ac4447166b30f56f11
peetck <peekung602@gmail.com>
2019-12-12 22:23:58 +0700

    Update ExceptionDemo.java

--- OOP Lab10/ExceptionDemo.java
+++ OOP Lab10/ExceptionDemo.java
@@ -12,14 +12,10 @@ import java.util.*;
 public class ExceptionDemo {
     public static void main(String[] args){
         double x1, x2, a, b, c;
-        String inp;
-        Scanner scan = new Scanner(System.in);
         try{
-            inp = scan.nextLine();
-            String[] arr = inp.split(" ");
-            a = Double.parseDouble(arr[0]);
-            b = Double.parseDouble(arr[1]);
-            c = Double.parseDouble(arr[2]);
+            a = Double.parseDouble(args[0]);
+            b = Double.parseDouble(args[1]);
+            c = Double.parseDouble(args[2]);
             x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2*a);
             x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2*a);
             System.out.println("x1: " + x1);
@@ -32,6 +28,6 @@ public class ExceptionDemo {
             System.out.println("Please enter 3 numbers as a, b, and c respectively.");
         }
     }
-    
+
 }
 // NumberFormatException ArrayIndexOutOfBoundsException
